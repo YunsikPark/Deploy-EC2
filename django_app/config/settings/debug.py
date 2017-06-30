83cac669dcd031a002ab751b8b9eb65ca59897df
Yunsik Park <space214@gmail.com>
2017-06-30 16:06:23 +0900

    WSGI module 분리, STATID_DIR, STATICFILES_DIRS추가

--- django_app/config/settings/debug.py
+++ django_app/config/settings/debug.py
@@ -2,6 +2,14 @@ from .base import *
 
 config_secret_debug = json.loads(open(CONFIG_SECRET_DEBUG_FILE).read())
 
-DEBUG = True
+# Static URLs
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
+# 디버그 모드니깐 DEBUG는 True
+DEBUG = True
 ALLOWED_HOSTS = config_secret_debug['django']['allowed_hosts']
+
+
+print('@@@ DEBUG:', DEBUG)
+print('@@@ ALLOWED_HOSTS:', ALLOWED_HOSTS)
--- django_app/config/settings/deploy.py
+++ django_app/config/settings/deploy.py
@@ -2,6 +2,10 @@ from .base import *
 
 config_secret_deploy = json.loads(open(CONFIG_SECRET_DEPLOY_FILE).read())
 
+# 배포모드이기 때문에 DEBUG는 False
 DEBUG = False
-
 ALLOWED_HOSTS = config_secret_deploy['django']['allowed_hosts']
+
+
+print('@@@ DEBUG:', DEBUG)
+print('@@@ ALLOWED_HOSTS:' , ALLOWED_HOSTS)
